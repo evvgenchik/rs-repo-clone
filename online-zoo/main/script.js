dc0b11280f1cecda1050310ab16eac3edd3f1d44
Evgeniy Onishchenko <yevgenchikk@mail.ru>
2022-10-08 17:39:45 +0300

    fix: fix pets left button

--- online-zoo/main/script.js
+++ online-zoo/main/script.js
@@ -41,49 +41,89 @@ const containerPets = document.querySelector('.pets__container')
 const blockPets = (document.querySelectorAll('.block-pets'))
 const petsButtonNext = document.querySelector('.pets__button_next')
 const petsButtonPrev = document.querySelector('.pets__button_prev')
+const buttonsPets = document.querySelector('.pets').querySelectorAll('button')
 
 
-petsButtonNext.addEventListener('click', createNewSlide)
+buttonsPets.forEach((item) => item.addEventListener('click', createNewSlide))
 
-
-function createNewSlide() {
+function createNewSlide(e) {
+	let button = e.target;
 	let listPets = document.querySelector('.list-pets')
 	let widthPetsList = listPets.offsetWidth;
 	let nextSlide = document.createElement('div')
-	containerPets.append(nextSlide)
-	nextSlide.classList.add('pets__list', 'list-pets')
-	nextSlide.style.left = 20 + widthPetsList + 'rem'
-	changeNewSlide(nextSlide, listPets)
+
+	if (button.textContent == '→') {
+		containerPets.append(nextSlide)
+		containerPets.firstElementChild.style.left = 0;
+		containerPets.firstElementChild.style.right = '';
+		nextSlide.classList.add('pets__list', 'list-pets')
+		nextSlide.style.left = 20 + widthPetsList + 'rem'
+		changeNewSlide(nextSlide, listPets, button)
+	}
+	if (button.textContent == '←') {
+		containerPets.prepend(nextSlide)
+		containerPets.lastElementChild.style.right = 0;
+		containerPets.lastElementChild.style.left = '';
+		nextSlide.classList.add('pets__list', 'list-pets')
+		nextSlide.style.right = 20 + widthPetsList + 'rem'
+
+		changeNewSlide(nextSlide, listPets, button)
+	}
+
 }
 
-function changeNewSlide(newSlide, listPets) {
+function changeNewSlide(newSlide, listPets, button) {
+
 	let clonBlock = Array.from((listPets.cloneNode(true)).querySelectorAll('.block-pets'))
 	for (let i = clonBlock.length - 1; i > 0; i--) {
 		let j = Math.floor(Math.random() * (i + 1));
 		[clonBlock[i], clonBlock[j]] = [clonBlock[j], clonBlock[i]];
 	}
-	addIconsInSlide(newSlide, clonBlock, listPets);
+	addIconsInSlide(newSlide, clonBlock, listPets, button);
 }
 
 
-function addIconsInSlide(newSlide, clonBlock, listPets) {
+function addIconsInSlide(newSlide, clonBlock, listPets, button) {
+
 	for (let block of clonBlock) {
 		let b = block
 		newSlide.append(b)
 	}
-	goNewSlide(listPets);
+	goNewSlide(listPets, button);
 }
 
-function goNewSlide(listPets) {
-	petsButtonNext.removeEventListener('click', createNewSlide)
+function goNewSlide(listPets, button) {
+	buttonsPets.forEach((item) => item.removeEventListener('click', createNewSlide))
 	let allListsPets = document.querySelectorAll('.list-pets')
 	let widthPetsList = listPets.offsetWidth;
-	allListsPets[0].style.left = -widthPetsList + 'rem';
-	allListsPets[1].style.left = 0;
-	setTimeout((deletePreviousSlide), 2000)
+	if (button.textContent == '→') {
+		allListsPets[0].style.left = -widthPetsList + 'rem';
+		allListsPets[1].style.left = 0;
+	}
+	if (button.textContent == '←') {
+		allListsPets[0].style.right = 0;
+
+		allListsPets[1].style.right = -widthPetsList + 'rem';
+	}
+	setTimeout(() => deletePreviousSlide(button), 2000)
 }
-function deletePreviousSlide() {
-	let forRemove = containerPets.querySelector('.list-pets')
-	forRemove.remove()
-	petsButtonNext.addEventListener('click', createNewSlide)
+function deletePreviousSlide(button) {
+	if (button.textContent == '→') {
+		console.log('next');
+		containerPets.firstElementChild.remove()
+	}
+	if (button.textContent == '←') {
+		console.log('prev');
+		containerPets.lastElementChild.remove()
+	}
+	buttonsPets.forEach((item) => item.addEventListener('click', createNewSlide))
 }
+
+
+
+
+
+/*.........CARUSEL TESTIMONIALS.................*/
+
+
+
